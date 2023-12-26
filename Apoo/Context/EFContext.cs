01004705b01cd5ed2a67f629cbc10d4b42386f43
Iraikare Rodrigues de Santana Alves Vieira <iraikare.rodrigues@academico.ifrn.edu.br>
2023-12-26 11:09:31 -0300

    Adicionar arquivos de projeto.

---
+++ Apoo/Context/EFContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Apoo.Models;
+
+namespace Apoo.Context
+{
+    public class EFContext : DbContext
+    {
+        internal object itens;
+
+        public EFContext() : base("Asp_Net_MVC_CS")
+        {
+            Database.SetInitializer<EFContext>(new DropCreateDatabaseIfModelChanges<EFContext>());
+        }
+        public DbSet<Item> Itens { get; set; }
+ 
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //Database.SetInitializer<demoEntities>(null);
+            //modelBuilder.Entity<Cliente>().ToTable("Clientes");
+            //modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            base.OnModelCreating(modelBuilder);
+
+
+        }
+    }
+}
